d017fe97c004ac8f5c23e4e06b626259a5ab9c4a
imranyy <imranmatano6@gmail.com>
2023-02-05 19:08:20 +0300

    TouchableOpacity

--- App.js
+++ App.js
@@ -1,50 +1,37 @@
 import React, {useState} from 'react';
-import { StyleSheet,Platform, TextInput, View, Text, KeyboardAvoidingView, TouchableOpacity, ScrollView, Keyboard } from 'react-native';
-import Task from './components/Task';
+import { StyleSheet, View, Text, FlatList,TouchableOpacity} from 'react-native';
 
 export default function App() {
-  const [task,setTask]=useState();
-  const [taskItem,setTaskItem]=useState([]);
-
-  const handleText=()=>{
-    Keyboard.dismiss()
-    setTaskItem([...taskItem, task]);
-    setTask(null)
-  }
-  const completeTask=(index)=>{
-    let itemCopy=[...taskItem];
-    itemCopy.splice(index, 1);
-    setTaskItem(itemCopy);
+  const [task,setTask]=useState([
+    {name:'imran',id:1},
+    {name:'matano',id:2},
+    {name:'mbiti',id:3},
+    {name:'ngati',id:4},
+    {name:'ngati',id:5},
+    {name:'ngati',id:6},
+    {name:'ngati',id:7},
+    {name:'ngati',id:8},
+    {name:'ngati',id:9},
+  ]);
+  const  pressHandler=(id)=>{
+    setTask((prevTask)=>{
+      return prevTask.filter(tas=>tas.id!=id)
+    })
   }
   return (
     <View style={styles.container}>
-        <View style={styles.wrapper}>
-          <Text style={styles.sectionTitle}>Today's tasks</Text>
-
-          <ScrollView style={styles.items}>
-            {/* list items /tasks */}
-            {
-              taskItem.map((i,index)=>(
-                <TouchableOpacity key={index} onPress={()=>completeTask(index)}>
-                    <Task text={i}/>
-                </TouchableOpacity>
-              ))
-            }
-          </ScrollView>
-
-        </View>
-
-        <KeyboardAvoidingView 
-          behavior={Platform.OS==='io'? "padding":"height"}
-          style={styles.writeTaskWrapper}
-          >
-            <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text=>setTask(text)}/>
-            <TouchableOpacity onPress={()=>handleText()}>
-              <View style={styles.addWrapper}>
-                <Text style={styles.addText}>+</Text>
-              </View>
-            </TouchableOpacity>
-      </KeyboardAvoidingView>
+          <View style={styles.wrapper}>
+            <Text style={styles.sectionTitle}>Today's tasks</Text>
+              <FlatList
+              style={{marginVertical:20,}}
+              keyExtractor={(item)=>item.id}
+                data={task}
+                  renderItem={({item})=>(
+                     <TouchableOpacity style={styles.item} onPress={()=>pressHandler(item.id)}>
+                        <Text style={styles.text}>{item.name}</Text>
+                    </TouchableOpacity>
+                  )}/>
+          </View>
     </View>
   );
 }
@@ -54,45 +41,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E8EAED',
   },
-
   wrapper:{
-    paddingTop:80,
-    paddingHorizontal:20, 
+    marginTop:50,
   },
-  sectionTitle:{
-    fontSize:24,
-    fontWeight:'bold',
+  item:{
+    margin:10,
   },
-  items:{
-    marginTop:30,
-  },
-  writeTaskWrapper:{
-    position:'absolute',
-    bottom:20,
-    width:'100%',
-    flexDirection:'row',
-    alignItems:'center'
-  },
-  input:{
-    paddingVertical:10,
-    paddingHorizontal:15,
-    paddingLeft:10,
-    width:'80%',
-    backgroundColor:'#55BCF6',
-    borderRadius:20,
-    borderColor:'#C0C0C0',
-    borderWidth:1,
-
-  },
-  addWrapper:{
-    width:50,
-    height:50,
-    backgroundColor:'#55BCF6',
-    borderRadius:60,
-    justifyContent:'center',
-    alignItems:'center',
-    marginLeft:10,
-  },
-  addText:{},
+  text:{
+    color:'white',
+    padding:10,
+    backgroundColor:'#777'
+  }
 });
 
